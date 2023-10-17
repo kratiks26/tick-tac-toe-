1b4af032a78a25e57f061c762583e6cda23816bd
kratiks26 <kratiks26@gmail.com>
2023-10-17 15:26:54 +0530

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-// import UseReducer from './reducer';
-
-// import UseReducerYT from './useReducerYT';
-// import Demo from './demo';
-
-import UseRefTest1 from './useRefTest1';
-
-import UseRefTest2 from './useRefTest2';
 
 import TickTack from './tickTack/tickTack';
 
@@ -16,13 +8,6 @@ import TickTack from './tickTack/tickTack';
 function App() {
   return (
     <div className="App">
-      {/* <UseReducerYT/> */}
-
-      {/* <UseReducer/> */}
-
-      {/* <Demo/> */}
-      {/* <UseRefTest1/> */}
-      {/* <UseRefTest2/> */}
       <TickTack/>
      </div>
   );
